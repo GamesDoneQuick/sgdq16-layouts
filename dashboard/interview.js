764b8057bd215a0c1d80fba0977d37208786c297
Alex Van Camp <email@alexvan.camp>
2016-06-04 21:35:02 -0500

    interview: add interview lowerthird graphic

--- dashboard/interview.js
+++ dashboard/interview.js
@@ -1,49 +1,15 @@
 (function () {
 	'use strict';
 
-	const disabledCover = document.getElementById('cover');
-
-	const layoutState = nodecg.Replicant('layoutState');
-	layoutState.on('change', newVal => {
-		if (newVal.page === 'open') {
-			if (layoutState.value.currentLayout === 'interview') {
-				disabledCover.reason = null;
-			} else {
-				disabledCover.reason = 'badLayout';
-			}
-		} else {
-			disabledCover.reason = newVal.page;
-		}
-	});
-
-	/* ----- */
-
-	const interviewNames = nodecg.Replicant('interviewNames');
-	const take = document.getElementById('take');
-	take.addEventListener('click', () => {
-		interviewNames.value = [
-			document.getElementById('preview1').value,
-			document.getElementById('preview2').value,
-			document.getElementById('preview3').value,
-			document.getElementById('preview4').value
-		];
-	});
-
-	interviewNames.on('change', newVal => {
-		document.getElementById('program1').value = newVal[0];
-		document.getElementById('program2').value = newVal[1];
-		document.getElementById('program3').value = newVal[2];
-		document.getElementById('program4').value = newVal[3];
-	});
-
-	/* ------ */
 
 	const show = document.getElementById('show');
 	const hide = document.getElementById('hide');
 	const auto = document.getElementById('auto');
+	const interviewNames = nodecg.Replicant('interviewNames');
 	const showing = nodecg.Replicant('interviewLowerthirdShowing');
 
 	show.addEventListener('click', () => {
+		takeNames();
 		showing.value = true;
 	});
 
@@ -52,6 +18,7 @@
 	});
 
 	auto.addEventListener('click', () => {
+		takeNames();
 		nodecg.sendMessage('pulseInterviewLowerthird', 10);
 	});
 
@@ -75,4 +42,13 @@
 			hide.removeAttribute('disabled');
 		}
 	});
+
+	/**
+	 * Takes the names currently entered into the paper-inputs.
+	 * @returns {undefined}
+	 */
+	function takeNames() {
+		const paperInputs = Array.from(document.getElementsByTagName('paper-input'));
+		interviewNames.value = paperInputs.map(input => input.value);
+	}
 })();
